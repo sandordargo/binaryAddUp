81d9f8869d5edb0b914b12bd49f4a8671c138cdb
Sandor Dargo <sandordargo@users.noreply.github.com>
2019-11-12 08:44:08 +0100

    Add helping function to convert a number to a bitset
    As the add has to return an `int`, I made the assumption that size of int is 32 bits as it would be in most environments
    If I could change the the signature I would use uintXX_t

--- src/BinaryAddUp.h
+++ src/BinaryAddUp.h
@@ -2,7 +2,15 @@
 #ifndef BINARY_ADD_UP_BINARYADDUP_H
 #define BINARY_ADD_UP_BINARYADDUP_H
 
+#include <bitset>
+
+
 namespace BinaryAddUp {
+    std::bitset<32> intToBitset(int number) {
+        std::bitset<32> numberAsBits(number);
+        return numberAsBits;
+    }
+
     int add(int a, int b) {
         return a+b;
     }
--- tst/TestBinaryAddUp.cpp
+++ tst/TestBinaryAddUp.cpp
@@ -1,6 +1,16 @@
 #include "gtest/gtest.h"
 #include "BinaryAddUp.h"
 
+#include <bitset>
+
 TEST(BinaryAddUp, sumOfZeros_Is_Zero) {
     ASSERT_EQ(0, BinaryAddUp::add(0,0));
+}
+
+TEST(BinaryAddUp, convertZeroToBitsetOfZeros) {
+    ASSERT_EQ(std::bitset<32>("00000000000000000000000000000000"), BinaryAddUp::intToBitset(0));
+}
+
+TEST(BinaryAddUp, convertSevenToCorrespondingBitset) {
+    ASSERT_EQ(std::bitset<32>("00000000000000000000000000000111"), BinaryAddUp::intToBitset(7));
 }
