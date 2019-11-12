fef03d7bc71af1b92a4c5b2009808b26fbded273
Sandor Dargo <sandordargo@users.noreply.github.com>
2019-11-12 09:02:04 +0100

    Implement a full adder circuit
    More info about the full adder circuit at: https://en.wikipedia.org/wiki/Adder_(electronics)#Full_adder

--- src/BinaryAddUp.h
+++ src/BinaryAddUp.h
@@ -12,11 +12,15 @@ namespace BinaryAddUp {
     }
 
     int add(int a, int b) {
+        // Implements a full adder circuit
+        // More information at https://en.wikipedia.org/wiki/Adder_(electronics)#Full_adder
         auto aBits{intToBitset(a)};
         auto bBits{intToBitset(b)};
         auto resultBits{intToBitset(0)};
+        bool carryOver = false;
         for (size_t i=0; i<aBits.size(); ++i) {
-            resultBits[i] = aBits[i] or bBits[i];
+            resultBits[i] = (aBits[i] ^ bBits[i]) ^ carryOver;
+            carryOver = (aBits[i] and bBits[i]) or ((aBits[i] ^ bBits[i]) and carryOver);
         }
         return resultBits.to_ulong();
     }
--- tst/TestBinaryAddUp.cpp
+++ tst/TestBinaryAddUp.cpp
@@ -11,6 +11,10 @@ TEST(BinaryAddUp, addNumbersWithoutCarryOver) {
     ASSERT_EQ(5, BinaryAddUp::add(1,4));
 }
 
+TEST(BinaryAddUp, addNumbersWithCarryOvers) {
+    ASSERT_EQ(14, BinaryAddUp::add(11,3));
+}
+
 TEST(BinaryAddUp, convertZeroToBitsetOfZeros) {
     ASSERT_EQ(std::bitset<32>("00000000000000000000000000000000"), BinaryAddUp::intToBitset(0));
 }
